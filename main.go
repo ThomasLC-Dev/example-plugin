ee39305289492f0e5ea6cf98641308bd374dbabd
ThomasLC-Dev <thomas.le-corvec@outlook.com>
2024-07-20 01:33:01 +0200

    update

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type DefaultPlugin struct{}
+
+func (p *DefaultPlugin) Init() error {
+	fmt.Printf("Init plugin")
+	return nil
+}
+
+func (p *DefaultPlugin) Execute(args []string) error {
+	fmt.Printf("Execution")
+	fmt.Printf("Args : %+v", args)
+	return nil
+}
+
 func main() {
-	fmt.Println("Hello world !")
+	
 }
